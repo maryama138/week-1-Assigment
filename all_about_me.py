990070022bb9d5f5efaf7fbea2376d64cafa989a
maryama138 <maryamaadowhassanhassan@gmail.com>
2026-09-18 14:40:56 +0300

    Introduce personal information and course goals
    Fix variable usage in print statements and improve formatting.

---
+++ all_about_me.py
@@ -0,0 +1,11 @@
+# This program introduces me and my goal for the course.
+
+name = "maryama"
+age = 19
+favourite_language = "Python"
+goal = "To learn programming and become a skilled software developer"
+
+print(f"My name is {maryama}.")
+print(f"I am {age} 19.")
+print(f"My favourite programming language is {language is Python}.")
+print(f"My goal for this course is {my goal for this course is to learn programming and become a skilled software developer.}.")
